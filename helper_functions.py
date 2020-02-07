c0ef12788745ed041ce0faba3ea871fc20c793db
bouracha <bouracha@tcd.ie>
2020-02-07 10:42:19 +0000

    Helper Functions introduced and learning curves made- needs refractoring

---
+++ helper_functions.py
@@ -0,0 +1,109 @@
+import hashlib
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.preprocessing import OneHotEncoder
+
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256*test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+def split_train_crossvalidate(X, y, cv_ratio):
+    shuffled_indices = np.random.permutation(len(X))
+    cv_set_size = int(len(X) * cv_ratio)
+    cv_indices = shuffled_indices[:cv_set_size]
+    train_indices = shuffled_indices[cv_set_size:]
+
+    return X.iloc[train_indices], y.iloc[train_indices], X.iloc[cv_indices], y.iloc[cv_indices]
+
+
+def get_session(sess):
+    session = sess
+    while type(session).__name__ != 'Session':
+        session = session._sess
+    return session
+
+
+def y_to_onehot(y):
+    y = np.array(y)
+    encoder = OneHotEncoder()
+    y = encoder.fit_transform(y.reshape(-1, 1)).toarray()
+    return y
+
+##############
+#DeepLearning
+##############
+
+# Global variables.
+log_period_samples = 20000
+batch_size = 100
+
+# Import dataset with one-hot encoding of the class labels.
+def get_data():
+  return input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# Placeholders to feed train and test data into the graph.
+# Since batch dimension is 'None', we can reuse them both for train and eval.
+def get_placeholders():
+  x = tf.placeholder(tf.float32, [None, 114])
+  y_ = tf.placeholder(tf.float32, [None, 6])
+  return x, y_
+
+# Plot learning curves of experiments
+def plot_learning_curves(experiment_data):
+  # Generate figure.
+  fig, axes = plt.subplots(3, 3, figsize=(16,12))
+  st = fig.suptitle(
+      "Learning Curves for all Tasks and Hyper-parameter settings",
+      fontsize="x-large")
+  # Plot all learning curves.
+  for i, results in enumerate(experiment_data):
+    for j, (setting, train_accuracy, test_accuracy) in enumerate(results):
+      # Plot.
+      xs = [x * log_period_samples for x in range(1, len(train_accuracy)+1)]
+      axes[j, i].plot(xs, train_accuracy, label='train_accuracy')
+      axes[j, i].plot(xs, test_accuracy, label='test_accuracy')
+      # Prettify individual plots.
+      axes[j, i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+      axes[j, i].set_xlabel('Number of samples processed')
+      axes[j, i].set_ylabel('Epochs: {}, Learning rate: {}.  Accuracy'.format(*setting))
+      axes[j, i].set_title('Task {}'.format(i + 1))
+      axes[j, i].legend()
+  # Prettify overall figure.
+  plt.tight_layout()
+  st.set_y(0.95)
+  fig.subplots_adjust(top=0.91)
+  plt.show()
+
+# Generate summary table of results.
+def plot_summary_table(experiment_data):
+  # Fill Data.
+  cell_text = []
+  rows = []
+  columns = ['Setting 1', 'Setting 2', 'Setting 3']
+  for i, results in enumerate(experiment_data):
+    rows.append('Model {}'.format(i + 1))
+    cell_text.append([])
+    for j, (setting, train_accuracy, test_accuracy) in enumerate(results):
+      if test_accuracy != []:
+        cell_text[i].append(test_accuracy[-1])
+      else:
+        print('Warning: Something went wrong! Missing testing/training data')
+  # Generate Table.
+  fig=plt.figure(frameon=False)
+  ax = plt.gca()
+  the_table = ax.table(
+      cellText=cell_text,
+      rowLabels=rows,
+      colLabels=columns,
+      loc='center')
+  the_table.scale(1, 4)
+  # Prettify.
+  ax.patch.set_facecolor('None')
+  ax.xaxis.set_visible(False)
+  ax.yaxis.set_visible(False)
--- tensorflow_implementations/vanilla_autoencoder/vanilla_autoencoder.py
+++ tensorflow_implementations/vanilla_autoencoder/vanilla_autoencoder.py
@@ -6,6 +6,8 @@ from functools import partial
 
 from sklearn.datasets import fetch_openml
 
+from home.pn.PycharmProjects.autoencoders.helper_functions import *
+
 
 def get_mnist_data():
     print("Getting MNIST data..")
@@ -39,46 +41,60 @@ def plot_reconstructions(originals, reconstructions):
 
 if __name__ == '__main__':
 
+    experimental_tasks1 = []
+    settings = [(5, 0.0001), (10, 0.0001), (15, 0.0001)]
     train_data, test_data, m = get_mnist_data()
 
-    n = 28 * 28  # for MNIST
+    for (num_epochs, l2_reg) in settings:
 
-    learning_rate = 0.01
-    l2_reg = 0.0001
+        n = 28 * 28  # for MNIST
 
-    X = tf.placeholder(tf.float32, shape=[None, n])
+        learning_rate = 0.01
+        l2_reg = 0.0001
 
-    he_init = tf.contrib.layers.variance_scaling_initializer()
-    l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
-    ## Partial allows to use the function my_dense_layer with same set parameters each time
-    my_dense_layer = partial(tf.layers.dense, activation=tf.nn.elu, kernel_initializer=he_init, kernel_regularizer=l2_regularizer)
+        X = tf.placeholder(tf.float32, shape=[None, n])
 
-    hidden1 = my_dense_layer(X, 300)
-    hidden2 = my_dense_layer(hidden1, 150)
-    hidden3 = my_dense_layer(hidden2, 300)
-    outputs = my_dense_layer(hidden3, n, activation=None) ##Overwrite: no activation fn in last layer
+        he_init = tf.contrib.layers.variance_scaling_initializer()
+        l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
+        ## Partial allows to use the function my_dense_layer with same set parameters each time
+        my_dense_layer = partial(tf.layers.dense, activation=tf.nn.elu, kernel_initializer=he_init, kernel_regularizer=l2_regularizer)
 
-    reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))  # MSE
+        hidden1 = my_dense_layer(X, 300)
+        hidden2 = my_dense_layer(hidden1, 150)
+        hidden3 = my_dense_layer(hidden2, 300)
+        outputs = my_dense_layer(hidden3, n, activation=None) ##Overwrite: no activation fn in last layer
 
-    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    loss = tf.add_n([reconstruction_loss] + reg_losses)
+        reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))  # MSE
 
-    optimizer = tf.train.AdamOptimizer(learning_rate)
-    training_op = optimizer.minimize(loss)
+        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        loss = tf.add_n([reconstruction_loss] + reg_losses)
 
-    init = tf.global_variables_initializer()
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        training_op = optimizer.minimize(loss)
 
-    n_epochs = 5
-    batch_size = 150
-    with tf.Session()   as sess:
-        init.run()
-        for epoch in range(n_epochs):
-            print("Epoch:", epoch, "/", n_epochs)
-            n_batches = m//batch_size
-            for batch in range(n_batches):
-                X_batch = train_data[batch*batch_size: (batch + 1)*batch_size]
-                sess.run(training_op, feed_dict={X: X_batch})
+        init = tf.global_variables_initializer()
 
-        reconstructions = outputs.eval(feed_dict={X: test_data[0: 10]})
+        #Train
+        train_loss, test_loss = [], []
+        #num_epochs = 5
+        batch_size = 150
+        with tf.Session()   as sess:
+            init.run()
+            train_loss.append(loss.eval(session=sess, feed_dict={X: train_data}))
+            test_loss.append(loss.eval(session=sess, feed_dict={X: test_data}))
+            for epoch in range(num_epochs):
+                print("Epoch:", epoch, "/", num_epochs)
+                n_batches = m//batch_size
+                for batch in range(n_batches):
+                    X_batch = train_data[batch*batch_size: (batch + 1)*batch_size]
+                    sess.run(training_op, feed_dict={X: X_batch})
+                train_loss.append(loss.eval(session=sess, feed_dict={X: train_data}))
+                test_loss.append(loss.eval(session=sess, feed_dict={X: test_data}))
 
-    plot_reconstructions(test_data[0: 10], reconstructions)
+            reconstructions = outputs.eval(feed_dict={X: test_data[0: 10]})
+
+        experimental_tasks1.append(((num_epochs, l2_reg), train_loss, test_loss))
+
+        #plot_reconstructions(test_data[0: 10], reconstructions)
+
+    plot_learning_curves([experimental_tasks1])
