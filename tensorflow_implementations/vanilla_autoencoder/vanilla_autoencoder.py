1d50def80dec53064ae02e010c0318f75daa3243
bouracha <bouracha@tcd.ie>
2020-02-07 11:40:32 +0000

    Made a generic class for autoencoder- ran successfully

--- tensorflow_implementations/vanilla_autoencoder/vanilla_autoencoder.py
+++ tensorflow_implementations/vanilla_autoencoder/vanilla_autoencoder.py
@@ -22,6 +22,36 @@ def plot_reconstructions(originals, reconstructions):
         ax.set_axis_off()
     plt.show()
 
+class AUTOENCODER(object):
+
+    def __init__(self):
+        n = 28 * 28  # for MNIST
+
+        learning_rate = 0.01
+        l2_reg = 0.0001
+
+        self.X = tf.placeholder(tf.float32, shape=[None, n])
+
+        self.he_init = tf.contrib.layers.variance_scaling_initializer()
+        self.l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
+        ## Partial allows to use the function my_dense_layer with same set parameters each time
+        self.my_dense_layer = partial(tf.layers.dense, activation=tf.nn.elu, kernel_initializer=self.he_init,
+                             kernel_regularizer=self.l2_regularizer)
+
+        self.hidden1 = self.my_dense_layer(self.X, 300)
+        self.hidden2 = self.my_dense_layer(self.hidden1, 150)
+        self.hidden3 = self.my_dense_layer(self.hidden2, 300)
+        self.outputs = self.my_dense_layer(self.hidden3, n, activation=None)  ##Overwrite: no activation fn in last layer
+
+        self.reconstruction_loss = tf.reduce_mean(tf.square(self.outputs - self.X))  # MSE
+
+        self.reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        self.loss = tf.add_n([self.reconstruction_loss] + self.reg_losses)
+
+        self.optimizer = tf.train.AdamOptimizer(learning_rate)
+        self.training_op = self.optimizer.minimize(self.loss)
+
+
 if __name__ == '__main__':
 
     experimental_tasks1 = []
@@ -30,30 +60,7 @@ if __name__ == '__main__':
 
     for (num_epochs, l2_reg) in settings:
 
-        n = 28 * 28  # for MNIST
-
-        learning_rate = 0.01
-        l2_reg = 0.0001
-
-        X = tf.placeholder(tf.float32, shape=[None, n])
-
-        he_init = tf.contrib.layers.variance_scaling_initializer()
-        l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
-        ## Partial allows to use the function my_dense_layer with same set parameters each time
-        my_dense_layer = partial(tf.layers.dense, activation=tf.nn.elu, kernel_initializer=he_init, kernel_regularizer=l2_regularizer)
-
-        hidden1 = my_dense_layer(X, 300)
-        hidden2 = my_dense_layer(hidden1, 150)
-        hidden3 = my_dense_layer(hidden2, 300)
-        outputs = my_dense_layer(hidden3, n, activation=None) ##Overwrite: no activation fn in last layer
-
-        reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))  # MSE
-
-        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-        loss = tf.add_n([reconstruction_loss] + reg_losses)
-
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-        training_op = optimizer.minimize(loss)
+        model = AUTOENCODER()
 
         init = tf.global_variables_initializer()
 
@@ -63,21 +70,22 @@ if __name__ == '__main__':
         batch_size = 150
         with tf.Session()   as sess:
             init.run()
-            train_loss.append(loss.eval(session=sess, feed_dict={X: train_data}))
-            test_loss.append(loss.eval(session=sess, feed_dict={X: test_data}))
+            train_loss.append(model.loss.eval(session=sess, feed_dict={model.X: train_data}))
+            test_loss.append(model.loss.eval(session=sess, feed_dict={model.X: test_data}))
             for epoch in range(num_epochs):
                 print("Epoch:", epoch, "/", num_epochs)
                 n_batches = m//batch_size
                 for batch in range(n_batches):
                     X_batch = train_data[batch*batch_size: (batch + 1)*batch_size]
-                    sess.run(training_op, feed_dict={X: X_batch})
-                train_loss.append(loss.eval(session=sess, feed_dict={X: train_data}))
-                test_loss.append(loss.eval(session=sess, feed_dict={X: test_data}))
+                    sess.run(model.training_op, feed_dict={model.X: X_batch})
+                train_loss.append(model.loss.eval(session=sess, feed_dict={model.X: train_data}))
+                test_loss.append(model.loss.eval(session=sess, feed_dict={model.X: test_data}))
 
-            reconstructions = outputs.eval(feed_dict={X: test_data[0: 10]})
+            reconstructions = model.outputs.eval(feed_dict={model.X: test_data[0: 10]})
 
         experimental_tasks1.append(((num_epochs, l2_reg), train_loss, test_loss))
 
         #plot_reconstructions(test_data[0: 10], reconstructions)
 
     plot_learning_curves([experimental_tasks1])
+    #print(experimental_tasks1)
